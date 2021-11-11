843e74542a43633b4e4c137c46d16bd6333e3e56
Hubert Radzewicz <Hubert.rdza@gmail.com>
2021-11-11 21:13:11 +0100

    FIRST TASK DONE, without styles

--- src/userResponse/userResponse.js
+++ src/userResponse/userResponse.js
@@ -1,18 +1,50 @@
 import { useState } from "react";
 
-export function UserResponse(){
+export function UserResponse() {
 
     const [users, setUsers] = useState(undefined);
+    const [accounts, setAccounts] = useState(undefined);
+    let accountsMap;
+    let usersMap;
 
-
-    if(users === undefined){
+    if (users === undefined) {
         fetch("http://localhost:3000/users")
             .then(response => response.json())
             .then(data => setUsers(data));
     }
 
-    console.log(users);
-    return(
-        <h1>looo!</h1>
+    if (accounts === undefined) {
+        fetch("http://localhost:3000/bank-accounts")
+            .then(response => response.json())
+            .then(data => setAccounts(data));
+    }
+
+    if (users !== undefined) {
+        usersMap = users.map((element, index) =>
+            <div key={index}>
+                <ul>
+                    <li>Email: {element.email}</li>
+                </ul>
+            </div>
+        );
+    }
+
+
+    if (users !== undefined) {
+        accountsMap = accounts.map((element, index) =>
+            <div key={index}>
+                <ul>
+                    <li>Type: {element.type}</li>
+                    <li>Balance: {element.balance} zł</li>
+                </ul>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h2>{usersMap}</h2>
+            <h2>{accountsMap}</h2>
+        </div>
     )
 }
